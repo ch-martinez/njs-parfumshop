7fbd38ec82075c4601f2a249f99f33e55dd09d8f
Christian Martinez <chmartinez.dev@gmail.com>
2024-07-17 22:44:31 -0300

    refactor: formateador

--- src/utils/dataFormater.js
+++ src/utils/dataFormater.js
@@ -1,28 +1,133 @@
 const calcDiscount = (price, discount) => {
-    return discount === 0 ? 0 : (price * (100 - discount) / 100).toFixed()
+    return discount === 0 ? price : (price * (100 - discount) / 100).toFixed()
 }
+
+function formatDateToArgentina(isoString) {
+    // Crear un objeto Date a partir de la cadena ISO 8601
+    const date = new Date(isoString);
+
+    // Calcular la diferencia de zona horaria entre UTC y Argentina (UTC-3)
+    const offset = -3 * 60; // -3 horas en minutos
+
+    // Obtener la fecha y hora en Argentina
+    const argentinaTime = new Date(date.getTime() + (offset * 60 * 1000));
+
+    // Obtener los componentes de la fecha y la hora
+    const day = String(argentinaTime.getUTCDate()).padStart(2, '0');
+    const month = String(argentinaTime.getUTCMonth() + 1).padStart(2, '0'); // Los meses son base 0, por eso sumamos 1
+    const year = argentinaTime.getUTCFullYear();
+    const hours = String(argentinaTime.getUTCHours()).padStart(2, '0');
+    const minutes = String(argentinaTime.getUTCMinutes()).padStart(2, '0');
+
+    // Formatear la fecha y la hora según el formato requerido
+    return `${day}-${month}-${year}, ${hours}:${minutes}hs`;
+}
+
+const flagsToObject = (flags) => {
+    flagsStr = flags.toString()
+    let flagsObject = {
+        new: flagsStr[0] == 1 ? true : false,
+        promoted: flagsStr[1] == 1 ? true : false,
+        featured: flagsStr[2] == 1 ? true : false,
+        topSeller: flagsStr[3] == 1 ? true : false,
+    }
+    return flagsObject
+}
+
+function convertObjectToBinaryString(obj) {
+    // Ordenar las claves para garantizar el orden correcto
+    const keys = ['new', 'promoted', 'featured', 'topSeller'];
+    let binaryString = '';
+    
+    keys.forEach(key => {
+      binaryString += obj[key] ? '1' : '0';
+    });
+    
+    return(binaryString);
+  }
+
+const flagsToArray = (obj) => {
+    const keys = ['new', 'promoted', 'featured', 'topSeller'];
+    let binaryString = '';
+
+    keys.forEach(key => {
+      binaryString += obj[key] ? '1' : '0';
+    });
+
+    return(binaryString);
+}
+
+
+
+
+/* EXPORTS */
+const productFormFormater = (p) => {
+
+    const flags = {
+        new: p.flag_new == 'on' ? true : false,
+        promoted: p.flag_promoted == 'on' ? true : false,
+        featured: p.flag_featured == 'on' ? true : false,
+        topSeller: p.flag_topSeller == 'on' ? true : false,
+    }
+
+    const productForm = {
+        product_name: p.product_name,
+        product_description: p.product_description,
+        product_ml: Number(p.product_ml),
+        product_family: p.product_family,
+        product_gender: p.product_gender,
+        product_type: p.product_type,
+        //product_img: p.product_img,
+
+        product_price_list: Number(p.product_price_list),
+        product_price_sell: Number(p.product_price_sell),
+        product_discount: Number(p.product_discount),
+        payment_id: Number(p.payment_id),
+
+        product_sku: Number(p.product_sku),
+        product_stock: Number(p.product_stock),
+        product_status: p.product_status == 'on' ? true : false,
+        product_flags: flagsToArray(flags),
+
+        brand_id: Number(p.brand_id),
+        provider_id: Number(p.provider_id),
+    }
+    return productForm
+}
+
 
 const productFormarter = (product) => {
     return {
         product_id: product.product_id,
-        product_sku: product.product_sku,
-        product_new: product.product_new == 0 ? false : true,
-        product_status: product.product_status == 0 ? false : true,
-        product_img: product.product_img,
         product_name: product.product_name,
-        product_discount: product.product_discount,
-        product_payments: product.product_payments,
-        product_price: product.product_price,
-        product_price_discount: calcDiscount(product.product_price, product.product_discount),
-        product_price_payments: (product.product_price / product.product_payments).toFixed(),
         product_description: product.product_description,
         product_ml: product.product_ml,
+        product_family: product.product_family,
         product_gender: product.product_gender,
         product_type: product.product_type,
-        product_notes: product.product_notes == null ? 's/d' : product.product_notes,
+        product_img: product.product_img,
+
+        product_price_list: product.product_price_list,
+        product_price_sell: product.product_price_sell,
+        product_discount: product.product_discount,
+        product_price_discount: calcDiscount(product.product_price_sell, product.product_discount),
+        payment_quantity: product.payment_quantity,
+        product_price_payments: (product.product_price_sell / product.payment_quantity).toFixed(),
+
+        product_sku: product.product_sku,
         product_stock: product.product_stock,
-        brand_name: product.brand_name,
+        product_status: product.product_status == 0 ? false : true,
+        product_flags: flagsToObject(product.product_flags),
+
+        provider_id: product.provider_id,
+        provider_name: product.provider_name,
+
         brand_id: product.brand_id,
+        brand_name: product.brand_name,
+
+        product_created: formatDateToArgentina(product.product_created),
+        product_updated: formatDateToArgentina(product.product_updated),
+
     }
 }
 
@@ -86,6 +191,7 @@ const paymentsFormarter = (paymentsList) => {
 
 module.exports = {
     productFormarter,
+    productFormFormater,
     addressFormarter,
     productsListFormater
 }
