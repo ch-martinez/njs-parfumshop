4deab9c289980a92c85230c611f7d264f707beb9
Christian Martinez <chmartinez.dev@gmail.com>
2024-07-17 22:44:16 -0300

    delet

--- src/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-
-router.get('/login', authController.loginView)
-router.get('/recovery', authController.recoveryView)
-router.get('/register', authController.registerView)
-
-module.exports = router
