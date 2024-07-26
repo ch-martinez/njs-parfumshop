4de16fddef327810db37a3b224fc6f82ea3df043
Christian Martinez <chmartinez.dev@gmail.com>
2024-07-25 23:06:15 -0300

    feat: ruas de producto

---
+++ src/routes/productsRouter.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/productsController')
+
+/* Todos los productos */
+router.get('/all', controller.productsView)
+
+/* Nuevo producto */
+router.get('/new', controller.productNewView)
+router.post('/new', controller.productNewPost)
+
+/* Editar producto */
+router.get('/:id/edit', controller.productEditView)
+router.post('/:id/edit', controller.productEditPost)
+
+/* Eliminar producto */
+router.post('/:id/delete', controller.productDeletePost)
+
+/* Detalle del producto */
+router.get('/:id', controller.productDetailView)
+
+/* Cambiar el estado del producto */
+router.post('/:id/status/:status', controller.productStatusPost)
+
+
+module.exports = router
