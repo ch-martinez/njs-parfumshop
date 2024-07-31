3564e88a4df9eed7922917908f3602419762695f
Christian Martinez <chmartinez.dev@gmail.com>
2024-07-30 22:14:56 -0300

    feat: controlador principal

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,18 +1,10 @@
-const {getAllProductsMinFromDB} = require('../models/productsModel')
-const {productCardFormarter} = require('../utils/dataFormater')
-
-const homeView = async (req, res) => {
-    const data = {
-        title: "ParfumShop",
-        products: [],
-        productsAll: [],
-    }
-    const products = productCardFormarter(await getAllProductsMinFromDB())
-    data.products = [products[0], products[1]]
-    data.productsAll = products
-    res.render('pages/main/home/homePage', { layout: 'layouts/mainLayout', data })
-}
+const mainView = (req, res) => {
+    let data = {
+        title: 'Panel administración'
+    };
+    res.render('pages/mainPage', { layout: 'layouts/mainLayout', data });   
+};
 
 module.exports = {
-    homeView
-}
+    mainView,
+};
